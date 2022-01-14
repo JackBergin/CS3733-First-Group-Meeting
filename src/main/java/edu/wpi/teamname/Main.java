58b48222b1c59425b01538bd732239b9987d8ea6
“jack <81708456+JackBergin@users.noreply.github.com>
2022-01-14 16:52:42 -0500

    Jack's Name Update

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -5,6 +5,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Ben");
     System.out.println("Ilyas");
+    System.out.println("Jack");
     App.launch(App.class, args);
   }
 }
